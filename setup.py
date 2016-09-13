568d4bfecc9a28b0c33be1ef8ceabd98cb6ecceb
Aurojit Panda <apanda@cs.berkeley.edu>
2016-09-12 19:50:36 -0700

    Setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from setuptools import setup
+setup(name='logrot',
+      version='0.1',
+      description='Random log rotation',
+      url='',
+      author='Aurojit Panda (Berkeley NetSys Lab)',
+      author_email='apanda@cs.berkeley.edu',
+      license='',
+      packages=[],
+      scripts=['logrot', 'logrot-test'],
+      install_requires=[]
+      )
